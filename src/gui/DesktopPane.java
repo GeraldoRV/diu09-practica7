1e35130e871fca6f430560622d5515ed4792407f
Geraldo <Geraldo@LAPTOP-09QGLT5H>
2017-11-02 20:58:16 +0000

    Commit num 3 de Ger : Menu hecho y DesktopPane pasito a pasito

--- src/gui/DesktopPane.java
+++ src/gui/DesktopPane.java
@@ -5,7 +5,7 @@ package gui;
 import javax.swing.JDesktopPane;
 import javax.swing.JInternalFrame;
 
-public class Desktop extends JDesktopPane{
+public class DesktopPane extends JDesktopPane{
     public void closeAllFrames() {
         JInternalFrame[] ventanas = getAllFrames();
         for (JInternalFrame ventana : ventanas) {
